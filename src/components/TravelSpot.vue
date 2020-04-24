98fdfdc79214bd93b356d24dc9d94e383ff2d011
Wonjong Oh <jgyuity1289@gmail.com>
2020-04-24 16:47:41 +0900

    Build TravelList template

---
+++ src/components/TravelSpot.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="travelspot" v-on:click="showDetail">
+    <div>{{ spot.title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TravelSpot",
+  props: ["content"],
+  data() {
+    return {
+      spot: this.content
+    };
+  },
+  methods: {
+    showDetail() {
+      alert(this.spot.title);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
--- src/components/TravelSpotList.vue
+++ src/components/TravelSpotList.vue
@@ -1,11 +1,49 @@
 <template>
-  <div></div>
+  <div class="travelspot-list">
+    <div v-bind:key="spot.id" v-for="spot in spots">
+      <TravelSpot v-bind:content="spot" />
+    </div>
+  </div>
 </template>
 
 <script>
+import { fetchNews } from "../api";
+import TravelSpot from "./TravelSpot";
+
 export default {
-  name: "TravelSpotList"
+  name: "TravelSpotList",
+  data() {
+    return {
+      spots: []
+    };
+  },
+  components: {
+    TravelSpot
+  },
+  created() {
+    fetchNews()
+      .then(res => {
+        this.spots = res.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.travelspot-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  div div {
+    width: 200px;
+    height: 200px;
+    border: 1px solid grey;
+    padding: 20px;
+    margin: 15px;
+  }
+}
+</style>
