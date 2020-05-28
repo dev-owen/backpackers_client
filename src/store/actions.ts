f5ef395c8c6b23b9766fe452afc3a3521b988740
Wonjong Oh <jgyuity1289@gmail.com>
2020-05-29 00:33:23 +0900

    put mock spots

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,11 +1,19 @@
 import { fetchSpots, fetchAllUsers } from "@/api";
 
 export default {
-  FETCH_SPOTS({ commit }: { commit: any }) {
-    return fetchSpots().then(response => {
-      commit("SET_SPOTS", response.data);
-      return response;
-    });
+  // Promise
+  // FETCH_SPOTS({ commit }: { commit: any }) {
+  //   return fetchSpots().then(response => {
+  //     commit("SET_SPOTS", response.data);
+  //     return response;
+  //   });
+  // },
+
+  // async
+  async FETCH_SPOTS({ commit }: { commit: any }) {
+    const response = await fetchSpots();
+    commit("SET_SPOTS", response.data);
+    return response;
   },
   FETCH_ALL_USERS({ commit }: { commit: any }) {
     return fetchAllUsers().then(response =>
